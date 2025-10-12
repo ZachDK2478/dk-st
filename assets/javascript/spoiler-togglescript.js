0fa0be910dd43f5adae7dd6fe058fcfc890301b7
ZachDK <zachariahspirit@gmail.com>
2025-10-12 15:04:39 -0400

    Work on spoiler button

--- assets/javascript/spoiler-togglescript.js
+++ assets/javascript/spoiler-togglescript.js
@@ -1,5 +1,21 @@
 const button = document.getElementById('spoiler-button');
+let isChecked = true;
+
 button.addEventListener('click', () => {
-const spoilers = document.querySelectorAll('.spoiler-text');
-spoilers.forEach(el => el.classList.toggle('revealed'));
+  isChecked = !isChecked;
+
+  button.src = isChecked
+    ? 'assets/images/sys/spoiler-checked.png'
+    : 'assets/images/sys/spoiler-unchecked.png';
+
+  const spoilers = document.querySelectorAll('.spoiler-text');
+  spoilers.forEach(el => {
+    if (isChecked) {
+      el.classList.remove('revealed');
+      el.classList.add('blurred');
+    } else {
+      el.classList.remove('blurred');
+      el.classList.add('revealed');
+    }
+  });
 });
